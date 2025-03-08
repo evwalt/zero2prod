abdd10949931b9aa2dff10a98c80891e0e57c6ad
evwalt <ewpw5678@gmail.com>
2025-03-07 17:02:13 -0800

    chore: commit and push autoformatted changes from long ago

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 
 async fn health_check() -> HttpResponse {
@@ -5,11 +6,8 @@ async fn health_check() -> HttpResponse {
 }
 
 pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
-            App::new()
-                .route("/health_check", web::get().to(health_check))
-        })
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .bind("127.0.0.1:8000")?
-        .run()
-        .await
+        .run();
+    Ok(server)
 }
